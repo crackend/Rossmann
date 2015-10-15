415a897754a7bb0d74e06d9d565f123121a1d64e
Georgios Douzas <georgiosdouzas@MacBook-Air-toutes-Georgios.local>
2015-10-15 20:36:12 +0300

    Changed dates to numeric

--- preprocess.R
+++ preprocess.R
@@ -5,7 +5,7 @@ store <- read.csv("data/store.csv")
 
 # Store table preprocessing
 notMissInd <- !is.na(store$CompetitionOpenSinceYear)
-store$CompetitionOpenSinceDate <-NA
+store$CompetitionOpenSinceDate <- NA
 store$CompetitionOpenSinceDate[notMissInd] <- as.Date(paste("1", 
                                                             store$CompetitionOpenSinceMonth[notMissInd], 
                                                             store$CompetitionOpenSinceYear[notMissInd], 
@@ -13,7 +13,7 @@ store$CompetitionOpenSinceDate[notMissInd] <- as.Date(paste("1",
                                                       "%d/%m/%Y")
 store$Promo2 <- factor(store$Promo2)
 notMissInd <- store$Promo2 == 1
-store$Promo2SinceDate <-NA
+store$Promo2SinceDate <- NA
 store$Promo2SinceDate[notMissInd] <- as.Date(store$Promo2SinceWeek[notMissInd] * 7, 
                                              origin = as.Date(as.character(store$Promo2SinceYear[notMissInd]), "%Y"))
 levels(store$PromoInterval) <- c(NA, 1:3)
@@ -24,8 +24,8 @@ facNames <- c("DayOfWeek", "Open", "Promo", "StateHoliday", "SchoolHoliday")
 train[, facNames] <- data.frame(apply(train[, facNames], 2, as.factor))
 test[, facNames] <- data.frame(apply(test[, facNames], 2, as.factor))
 levels(test$StateHoliday) <- levels(train$StateHoliday)
-train$Date <- as.Date(train$Date, "%Y-%m-%d")
-test$Date <- as.Date(test$Date, "%Y-%m-%d")
+train$Date <- as.numeric(as.Date(train$Date, "%Y-%m-%d"))
+test$Date <- as.numeric(as.Date(test$Date, "%Y-%m-%d"))
 
 # Join tables
 train <- merge(train, store)[, -1]
