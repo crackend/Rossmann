83b7a4ea610943ff70a4d305341d4a4b7aecb164
Georgios Douzas <georgiosdouzas@MacBook-Air-toutes-Georgios.local>
2015-10-18 02:08:47 +0300

    Renamed

--- evalMetric.R
+++
@@ -1,8 +0,0 @@
-# Evaluation metric
-rmspe <- function(preds, obs) {
-  nonZeroInd <- obs > 0
-  preds <- preds[nonZeroInd]
-  obs <- obs[nonZeroInd]
-  err <- sqrt(sum(((preds - obs) / obs) ^ 2, na.rm = TRUE) / length(obs))
-  return(RMSPE = err)
-}
